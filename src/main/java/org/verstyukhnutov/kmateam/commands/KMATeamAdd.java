cbbbce7a2d7477f3131b772226817cb979e29479
TeksPeks <verstyukm@gmail.com>
2024-03-03 18:25:53 +0200

    refactoring: get rid of nested if statements

--- src/main/java/org/verstyukhnutov/kmateam/commands/KMATeamAdd.java
+++ src/main/java/org/verstyukhnutov/kmateam/commands/KMATeamAdd.java
@@ -53,113 +53,111 @@ public class KMATeamAdd implements Runnable {
     }
 
     private void addTeacher() {
-        if (teacher != null) {
-            if (teacher.getDepartment() != null && teacher.getName() != null) {
-                if (!Validation.isValidName(teacher.getName())) {
-                    Debug.error("Invalid teacher name!");
-                    return;
-                }
-
-                Department dep = null;
-                for (Faculty fac : Program.university.getFaculties().values()) {
-                    dep = fac.getDepartment(teacher.getDepartment());
-                    if (dep != null)
-                        break;
-                }
-
-                if (dep != null) {
-                    if (dep.addTeacher(teacher)) {
-                        Debug.info("Teacher is successfully added.");
-                    } else {
-                        Debug.error("The teacher already exists!");
-                    }
-                } else {
-                    Debug.error("No such department `" + teacher.getDepartment() + "`");
-                }
-                return;
-            }
+        if (teacher == null) {
+            Debug.error("All teacher options must be specified!");
+            return;
         }
 
-        Debug.error("All teacher options must be specified!");
+        if (teacher.getDepartment() == null || teacher.getName() == null) {
+            Debug.error("All teacher options must be specified!");
+            return;
+        }
+
+        if (!Validation.isValidName(teacher.getName())) {
+            Debug.error("Invalid teacher name!");
+            return;
+        }
+
+        Department dep = null;
+        for (Faculty fac : Program.university.getFaculties().values()) {
+            dep = fac.getDepartment(teacher.getDepartment());
+            if (dep != null)
+                break;
+        }
+
+        if (dep == null) {
+            Debug.error("No such department `" + teacher.getDepartment() + "`");
+            return;
+        }
+
+        if (dep.addTeacher(teacher)) {
+            Debug.info("Teacher is successfully added.");
+        } else {
+            Debug.error("The teacher already exists!");
+        }
     }
 
     private void addStudent() {
-        if (student != null) {
-            if (student.getName() != null &&
-                student.getCourse() != 0 &&
-                student.getGroup() != 0 &&
-                student.getDepartment() != null)
-            {
-                if (!Validation.isValidName(student.getName())) {
-                    Debug.error("Invalid student name! Name must be of format `P_I_B`");
-                    return;
-                }
-                if (!Validation.isValidCourse(student.getCourse())) {
-                    Debug.error("Invalid student course! Course must be in range 1-4");
-                    return;
-                }
-                if (!Validation.isValidGroup(student.getGroup())) {
-                    Debug.error("Invalid student group! Group must be in range 1-6");
-                    return;
-                }
-
-                Department dep = null;
-                for (Faculty fac : Program.university.getFaculties().values()) {
-                    dep = fac.getDepartment(student.getDepartment());
-                    if (dep != null)
-                        break;
-                }
-
-                if (dep != null) {
-                    if (dep.addStudent(student)) {
-                        Debug.info("Student is successfully added.");
-                    } else {
-                        Debug.error("The student already exists!");
-                    }
-                } else {
-                    Debug.error("No such department `" + student.getDepartment() + "`");
-                }
-                return;
-            }
+        if (student == null || student.getName() == null || student.getCourse() == 0 ||
+            student.getGroup() == 0 || student.getDepartment() == null) {
+            Debug.error("All student options must be specified!");
+            return;
         }
 
-        Debug.error("All student options must be specified!");
+        if (!Validation.isValidName(student.getName())) {
+            Debug.error("Invalid student name! Name must be of format `P_I_B`");
+            return;
+        }
+
+        if (!Validation.isValidCourse(student.getCourse())) {
+            Debug.error("Invalid student course! Course must be in range 1-4");
+            return;
+        }
+
+        if (!Validation.isValidGroup(student.getGroup())) {
+            Debug.error("Invalid student group! Group must be in range 1-6");
+            return;
+        }
+
+        Department dep = null;
+        for (Faculty fac : Program.university.getFaculties().values()) {
+            dep = fac.getDepartment(student.getDepartment());
+            if (dep != null)
+                break;
+        }
+
+        if (dep == null) {
+            Debug.error("No such department `" + student.getDepartment() + "`");
+            return;
+        }
+
+        if (dep.addStudent(student)) {
+            Debug.info("Student is successfully added.");
+        } else {
+            Debug.error("The student already exists!");
+        }
     }
 
     private void addDepartment() {
-        if (department != null) {
-            if (department.getName() != null && department.getFaculty() != null) {
-                Faculty fac = Program.university.getFaculty(department.getFaculty());
-                if (fac != null) {
-                    if (fac.addDepartment(department)) {
-                        Debug.info("Department is successfully added.");
-                    } else {
-                        Debug.error("The department already exists!");
-                    }
-                } else {
-                    Debug.error("No such faculty `" + department.getFaculty() + "`");
-                }
-                return;
-            }
+        if (department == null || department.getName() == null || department.getFaculty() == null) {
+            Debug.error("All department options must be specified!");
+            return;
         }
 
-        Debug.error("All department options must be specified!");
+        Faculty fac = Program.university.getFaculty(department.getFaculty());
+
+        if (fac == null) {
+            Debug.error("No such faculty `" + department.getFaculty() + "`");
+            return;
+        }
+
+        if (fac.addDepartment(department)) {
+            Debug.info("Department is successfully added.");
+        } else {
+            Debug.error("The department already exists!");
+        }
     }
 
     private void addFaculty() {
-        if (faculty != null) {
-            if (faculty.getName() != null) {
-                if (Program.university.addFaculty(faculty)) {
-                    Debug.info("Faculty is successfully added.");
-                } else {
-                    Debug.error("The faculty already exists!");
-                }
-
-                return;
-            }
+        if (faculty == null || faculty.getName() == null) {
+            Debug.error("All faculty options must be specified!");
+            return;
         }
 
-        Debug.error("All faculty options must be specified!");
+        if (Program.university.addFaculty(faculty)) {
+            Debug.info("Faculty is successfully added.");
+        } else {
+            Debug.error("The faculty already exists!");
+        }
     }
-
 }
--- src/main/java/org/verstyukhnutov/kmateam/commands/KMATeamEdit.java
+++ src/main/java/org/verstyukhnutov/kmateam/commands/KMATeamEdit.java
@@ -61,193 +61,188 @@ public class KMATeamEdit implements Runnable {
     }
 
     private void editTeacher() {
-        if (teacher != null) {
-            Department borrowedDepartment = null;
-            Teacher teacherToEdit = null;
-            for (Faculty fac : Program.university.getFaculties().values()) {
-                for (Department dep : fac.getDepartments().values()) {
-                    for (String teacherName : dep.getTeachers().keySet()) {
-                        if (teacherName.equals(manipulateName)) {
-                            borrowedDepartment = dep;
-                            teacherToEdit = dep.getTeacher(teacherName);
-                        }
-                    }
-                }
-            }
-
-            if (teacherToEdit != null) {
-
-                if (teacher.getDepartment() != null) {
-                    Debug.error("Cannot edit teacher's department! Use `delete` command instead.");
-                    return;
-                }
-
-                if (teacher.getName() != null) {
-                    if (!Validation.isValidName(teacher.getName())) {
-                        Debug.error("Invalid teacher name! Name must be of format `P_I_B`");
-                        return;
-                    }
-
-                    borrowedDepartment.removeTeacher(teacherToEdit.getName());
-                    teacherToEdit.setName(teacher.getName());
-                    borrowedDepartment.addTeacher(teacherToEdit);
-                }
-
-            } else {
-                Debug.error("No such student `" + manipulateName + "`!");
-                return;
-            }
-
-            Debug.info("Teacher is successfully edited.");
+        if (teacher == null) {
+            Debug.error("Teacher must have at least 1 field to edit!");
             return;
         }
 
-        Debug.error("Teacher must have at least 1 field to edit!");
+        Department borrowedDepartment = null;
+        Teacher teacherToEdit = null;
+
+        for (Faculty fac : Program.university.getFaculties().values()) {
+            for (Department dep : fac.getDepartments().values()) {
+                if (dep.getTeachers().containsKey(manipulateName)) {
+                    borrowedDepartment = dep;
+                    teacherToEdit = dep.getTeacher(manipulateName);
+                    break;
+                }
+            }
+            if (borrowedDepartment != null) {
+                break;
+            }
+        }
+
+        if (teacherToEdit == null) {
+            Debug.error("No such student `" + manipulateName + "`!");
+            return;
+        }
+
+        if (teacher.getDepartment() != null) {
+            Debug.error("Cannot edit teacher's department! Use `delete` command instead.");
+            return;
+        }
+
+        if (teacher.getName() != null) {
+            if (!Validation.isValidName(teacher.getName())) {
+                Debug.error("Invalid teacher name! Name must be of format `P_I_B`");
+                return;
+            }
+
+            borrowedDepartment.removeTeacher(teacherToEdit.getName());
+            teacherToEdit.setName(teacher.getName());
+            borrowedDepartment.addTeacher(teacherToEdit);
+        }
+
+        Debug.info("Teacher is successfully edited.");
     }
 
     private void editStudent() {
-        if (student != null) {
-            Department borrowedDepartment = null;
-            Student studentToEdit = null;
-            for (Faculty fac : Program.university.getFaculties().values()) {
-                for (Department dep : fac.getDepartments().values()) {
-                    for (String studentName : dep.getStudents().keySet()) {
-                        if (studentName.equals(manipulateName)) {
-                            borrowedDepartment = dep;
-                            studentToEdit = dep.getStudent(studentName);
-                        }
-                    }
-                }
-            }
-
-            if (studentToEdit != null) {
-
-                if (student.getDepartment() != null) {
-                    Debug.error("Cannot edit student's department! Use `delete` command instead.");
-                    return;
-                }
-
-                if (student.getGroup() != 0) {
-                    if (!Validation.isValidGroup(student.getGroup())) {
-                        Debug.error("Invalid student group! Group must be in range 1-6");
-                        return;
-                    }
-
-                    studentToEdit.setGroup(student.getGroup());
-                }
-
-                if (student.getCourse() != 0) {
-                    if (!Validation.isValidCourse(student.getCourse())) {
-                        Debug.error("Invalid student course! Course must be in range 1-6");
-                        return;
-                    }
-
-                    studentToEdit.setCourse(student.getCourse());
-                }
-
-                if (student.getName() != null) {
-                    if (!Validation.isValidName(student.getName())) {
-                        Debug.error("Invalid student name! Name must be of format `P_I_B`");
-                        return;
-                    }
-
-                    borrowedDepartment.removeStudent(studentToEdit.getName());
-                    studentToEdit.setName(student.getName());
-                    borrowedDepartment.addStudent(studentToEdit);
-                }
-
-            } else {
-                Debug.error("No such student `" + manipulateName + "`!");
-                return;
-            }
-
-            Debug.info("Student is successfully edited.");
+        if (student == null) {
+            Debug.error("Student must have at least 1 field to edit!");
             return;
         }
 
-        Debug.error("Student must have at least 1 field to edit!");
+        Department borrowedDepartment = null;
+        Student studentToEdit = null;
+
+        for (Faculty fac : Program.university.getFaculties().values()) {
+            for (Department dep : fac.getDepartments().values()) {
+                if (dep.getStudents().containsKey(manipulateName)) {
+                    borrowedDepartment = dep;
+                    studentToEdit = dep.getStudent(manipulateName);
+                    break;
+                }
+            }
+            if (borrowedDepartment != null) {
+                break;
+            }
+        }
+
+        if (studentToEdit == null) {
+            Debug.error("No such student `" + manipulateName + "`!");
+            return;
+        }
+
+        if (student.getDepartment() != null) {
+            Debug.error("Cannot edit student's department! Use `delete` command instead.");
+            return;
+        }
+
+        if (student.getGroup() != 0 && !Validation.isValidGroup(student.getGroup())) {
+            Debug.error("Invalid student group! Group must be in range 1-6");
+            return;
+        }
+
+        if (student.getCourse() != 0 && !Validation.isValidCourse(student.getCourse())) {
+            Debug.error("Invalid student course! Course must be in range 1-6");
+            return;
+        }
+
+        if (student.getName() != null && !Validation.isValidName(student.getName())) {
+            Debug.error("Invalid student name! Name must be of format `P_I_B`");
+            return;
+        }
+
+        if (student.getGroup() != 0) {
+            studentToEdit.setGroup(student.getGroup());
+        }
+
+        if (student.getCourse() != 0) {
+            studentToEdit.setCourse(student.getCourse());
+        }
+
+        if (student.getName() != null) {
+            borrowedDepartment.removeStudent(studentToEdit.getName());
+            studentToEdit.setName(student.getName());
+            borrowedDepartment.addStudent(studentToEdit);
+        }
+
+        Debug.info("Student is successfully edited.");
     }
 
     private void editDepartment() {
-        if (department != null) {
-            Faculty borrowedFaculty = null;
-            Department departmentToEdit = null;
-            for (Faculty fac : Program.university.getFaculties().values()) {
-                for (String departmentName : fac.getDepartments().keySet()) {
-                    if (departmentName.equals(manipulateName)) {
-                        borrowedFaculty = fac;
-                        departmentToEdit = fac.getDepartment(departmentName);
-                    }
-                }
+        if (department == null) {
+            Debug.error("Department must have at least 1 field to edit!");
+            return;
+        }
+
+        Faculty borrowedFaculty = null;
+        Department departmentToEdit = null;
+
+        for (Faculty fac : Program.university.getFaculties().values()) {
+            if (fac.getDepartments().containsKey(manipulateName)) {
+                borrowedFaculty = fac;
+                departmentToEdit = fac.getDepartment(manipulateName);
+                break;
             }
+        }
 
-            if (departmentToEdit != null) {
+        if (departmentToEdit == null) {
+            Debug.error("No such department `" + manipulateName + "`!");
+            return;
+        }
 
-                if (department.getFaculty() != null) {
-                    Debug.error("Cannot edit department's faculty! Use `delete` command instead.");
-                    return;
-                }
+        if (department.getFaculty() != null) {
+            Debug.error("Cannot edit department's faculty! Use `delete` command instead.");
+            return;
+        }
 
-                if (department.getName() != null) {
-                    borrowedFaculty.removeDepartment(departmentToEdit.getName());
-
-                    departmentToEdit.setName(department.getName());
-
-                    for (Student student : departmentToEdit.getStudents().values()) {
-                        student.setDepartment(department.getName());
-                    }
-
-                    for (Teacher teacher : departmentToEdit.getTeachers().values()) {
-                        teacher.setDepartment(department.getName());
-                    }
-
-                    borrowedFaculty.addDepartment(departmentToEdit);
-                }
-
-            } else {
-                Debug.error("No such department `" + manipulateName + "`!");
-                return;
-            }
-
+        if (department.getName() == null) {
             Debug.info("Department is successfully edited.");
             return;
         }
 
-        Debug.error("Department must have at least 1 field to edit!");
+        borrowedFaculty.removeDepartment(departmentToEdit.getName());
+        departmentToEdit.setName(department.getName());
+
+        for (Student student : departmentToEdit.getStudents().values()) {
+            student.setDepartment(department.getName());
+        }
+
+        for (Teacher teacher : departmentToEdit.getTeachers().values()) {
+            teacher.setDepartment(department.getName());
+        }
+
+        borrowedFaculty.addDepartment(departmentToEdit);
+        Debug.info("Department is successfully edited.");
     }
 
     private void editFaculty() {
-        if (faculty != null) {
-            Faculty facultyToEdit = null;
-            for (String facultyName : Program.university.getFaculties().keySet()) {
-                if (facultyName.equals(manipulateName)) {
-                    facultyToEdit = Program.university.getFaculty(facultyName);
-                }
-            }
+        if (faculty == null) {
+            Debug.error("Faculty must have at least 1 field to edit!");
+            return;
+        }
 
-            if (facultyToEdit != null) {
+        Faculty facultyToEdit = Program.university.getFaculty(manipulateName);
 
-                if (faculty.getName() != null) {
-                    Program.university.removeFaculty(facultyToEdit.getName());
+        if (facultyToEdit == null) {
+            Debug.error("No such faculty `" + manipulateName + "`!");
+            return;
+        }
 
-                    facultyToEdit.setName(faculty.getName());
-                    for (Department dep : facultyToEdit.getDepartments().values()) {
-                        dep.setFaculty(faculty.getName());
-                    }
-
-                    Program.university.addFaculty(facultyToEdit);
-                }
-
-            } else {
-                Debug.error("No such faculty `" + manipulateName + "`!");
-                return;
-            }
-
+        if (faculty.getName() == null) {
             Debug.info("Faculty is successfully edited.");
             return;
         }
 
-        Debug.error("Faculty must have at least 1 field to edit!");
-    }
+        Program.university.removeFaculty(facultyToEdit.getName());
+        facultyToEdit.setName(faculty.getName());
 
+        for (Department dep : facultyToEdit.getDepartments().values()) {
+            dep.setFaculty(faculty.getName());
+        }
+
+        Program.university.addFaculty(facultyToEdit);
+        Debug.info("Faculty is successfully edited.");
+    }
 }
